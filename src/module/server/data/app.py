a2c1cdd4a5ed0b1b77b9310240d39aa1af1b6843
Physlex <al.willet02@gmail.com>
2022-07-30 11:50:31 -0600

    Folder Restructure

---
+++ src/module/server/data/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from client.muse_import import Stream
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route("/")
+def postME():
+    data = request.get_json()
+    data = jsonify(data)
+    return data
+
+if __name__ == "__main__" :
+    my_stream = Stream()
+    my_stream.connect()
+    app.run(debug=True)
